e85ff582c5f94d83277a8f665bbdf1ec19e0025f
Vic <91158876+somevy@users.noreply.github.com>
2023-08-13 23:37:25 -0400

    add start page for AL-ZONE

--- menu/sketch.js
+++ menu/sketch.js
@@ -1,25 +1,6 @@
-let gameState = "menu";
-
 function setup() {
   createCanvas(1263, 617);
   background(220);
-
-  // menuGrid.child(cnv);
-
-  // let memoryText = createDiv('Memory Game');
-  // memoryText.class('game-header');
-  // memoryText.style('background')
-  // memoryText.position(windowWidth/2.52, windowHeight/9);
-  
-  // let preview = createDiv();
-  // preview.class("preview");
-  // preview.position(250, 160);
-
-
-  // let playButton = createButton('');
-  // playButton.child('preview');
-  // playButton.class('play-button');
-  // playButton.mousePressed(statePlay);
 }
 
 function draw() {
