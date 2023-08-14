cb67a94922486c62ac9890fa7fbf1dc2cea47cf6
Arav-Mathur <76656254+Arav-Mathur@users.noreply.github.com>
2023-08-14 10:23:33 -0400

    Add files via upload

--- Alzheimer’s/Bricks.js
+++ Alzheimer’s/Bricks.js
@@ -11,7 +11,9 @@ class Bricks {
      // this.numbers = []
       this.buttons = []
       //this.buttonText;
+      this.clickstate = 0;
       this.gamestate = 0 //play
+      this.rect = [];
     }
 
   getloc() {
@@ -28,37 +30,52 @@ class Bricks {
       this.xvalus.splice(this.xvalus.indexOf(xv),1)
       this.yvalus.splice(this.yvalus.indexOf(yv),1)
     }
-    //get numbers
-    // for(let i = 1; i<this.level+1; i++){
-    //   this.numbers.push(i);
-    // }    
     //get all vals
     for(let i = 0; i< this.level; i++){
-        this.pos.push([this.x[i],this.y[i]//,this.numbers[i]
-        ])
-        //this.buttonText = i+1
-        this.buttons[i] = createButton(i+1//this.buttonText
-        ); 
-        this.buttons[i].class('memory-bricks'); //added class to bricks
-        this.buttons[i].style('label','bvh');
+        this.pos.push([this.x[i],this.y[i]])
+
+        this.buttons[i] = createButton(i+1); 
+        this.buttons[i].class('memory-bricks');
         this.buttons[i].position(this.pos[i][0],this.pos[i][1])
         this.buttons[i].size(width / this.level - 10, height / this.level - 10)
+        this.buttons[i].hide()
+        this.buttons[i].id(i)    
+        // this.rect[i] = createButton("")//this.pos[i][0]+this.buttons[i].width/2,this.pos[i][1] + this.buttons[i].height/2,50,50
+        // this.rect[i].class('memory-bricks');
+
+        // this.rect[i].size(50,50)
+        // this.rect[i].position(this.pos[i][0]+this.buttons[i].width/2 -this.rect[i].width/2,this.pos[i][1] + this.buttons[i].height/2 - this.rect[i].height/2)
+        // this.rect[i].hide()
     }
   }
   showlevel() {
+    let id = document.getElementsByClassName('memory-bricks')
     for (let i = 0; i < this.buttons.length; i++) {
-      this.buttons[i].mouseClicked(() => this.clicked(i));
-      
+      this.buttons[i].show()
+      this.buttons[i].mouseClicked(() => this.clicked(i));    
+      if(this.clickstate == 1){
+        // this.rect[i].show(); 
+        // this.rect[i].mouseClicked(() => this.clicked(i)); 
+        id[i].innerHTML = ""
     }
+    }
+
   }
   clicked(i){
+    this.clickstate = 1;
     this.buttons[i].remove()
     this.buttons.splice(i,1);
+    // this.rect[i].remove()
+    // this.rect.splice(i,1);
     if(i > 0){//basically, this.buttons.indexOf(this.buttons[0]) = 0
       this.gamestate = 1; // u lose
     } else if(this.buttons.length == 0){
       this.gamestate = 2; // u win
-    }else {this.points++}
+    }else {
+      this.points++     
+      if(this.points>this.highscore){
+      this.highscore = this.points;
+    } }
   }
    reset(){
     this.x=[]
@@ -71,12 +88,20 @@ class Bricks {
       this.buttons[i].remove()
     }
     this.buttons = []
+    this.rect = []
+    this.clickstate = 0;
     this.getloc()
     this.showlevel() //play
 
+  }
+  hidebuttons(){
+    for(let i = 0; i < this.buttons.length; i++){
+      this.buttons[i].hide()
+      //this.rect[i].hide()
+    }
   }
    nextlevel(){
     this.level++
     this.reset()
   }
-}//make button text invisible
+}
--- Alzheimer’s/memoryGame.js
+++ Alzheimer’s/memoryGame.js
@@ -16,26 +16,38 @@ function setup() {
   nextB = createButton("Next Level")
   nextB.class("memory-option-buttons")
   nextB.center()
-  nextB.hide()
+  nextB.hide()    
+  score = createP("YOUR SCORE: "+ brick.points)
+  score.class("text-memory-game")
+  score.position(0,70)
+  score.center('horizontal')
+  score.id("p1")
+  score.hide()
+  highS = createP("YOUR HIGHSCORE: "+ brick.highscore)
+  highS.class("text-memory-game")
+  highS.id("p2")
+  highS.position(0,140)
+  highS.center('horizontal')
+  highS.hide()
 }
 
 function draw() {
   background(0);
   if (brick.gamestate >0){
     background(100);
-        textAlign(CENTER,CENTER)
-
-
+    textAlign(CENTER,CENTER)
     if (brick.gamestate == 1) {//lost
     restartB.show()
     restartB.mousePressed(restart)
     } else { // u win
       nextB.show()
       nextB.mousePressed(nextLevel)
-    }     
-
-    text("YOUR SCORE: "+ brick.points,width/2,(height/2)-70)
-    text("YOUR HIGHSCORE: "+ brick.highscore,width/2,(height/2)-70)
+    }
+    document.getElementById("p1").innerHTML = "YOUR SCORE: "+ brick.points;
+    document.getElementById("p2").innerHTML = "YOUR HIGHSCORE: "+ brick.highscore;
+    brick.hidebuttons()
+    score.show()
+    highS.show()
     backB.show()
   } else {//play
     hideall()
@@ -47,11 +59,14 @@ function hideall(){
   backB.hide()
   restartB.hide()
   nextB.hide()
+  score.hide()
+  highS.hide()
 }
 function restart(){  
-  brick.reset()
+  
   brick.points = 1;
   brick.level = 4;
+  brick.reset()
 }
 function nextLevel(){
   brick.nextlevel()
